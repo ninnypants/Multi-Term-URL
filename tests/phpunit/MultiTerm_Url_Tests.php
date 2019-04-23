e43eafc51759e98dc47fe68d8b8e5e0f3401be5f
Pete Nelson <pete@petenelson.com>
2019-04-22 22:09:53 -0500

    initial unit testing setup

---
+++ tests/phpunit/MultiTerm_Url_Tests.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Plugin\MultiTermUrl\Tests;
+
+class MultiTerm_Url_Tests extends \WP_UnitTestCase {
+
+	public function test_hello_world() {
+
+		$this->assertTrue( true );
+
+
+	}
+	
+}
